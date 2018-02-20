7ab309cb5c91cb5aa875403f21b37b971d99ecf9
Lovisa <liia@exuvo.se>
2018-02-20 13:13:00 +0100

    Add files via upload

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -15,10 +15,11 @@ A = matrixA
 B = matrixB
 A = A.T
 B = B.T
-x_train = A[0:15000,:]
-y_train = B[0:15000,:]
-x_test = A[15000:19800,:]
-y_test = B[15000:19800,:]
+train_len = 5000
+x_train = A[0:train_len,:]
+y_train = B[0:train_len,:]
+x_test = A[train_len:19800,:]
+y_test = B[train_len:19800,:]
 
 
 #model = Sequential()
@@ -26,49 +27,35 @@ y_test = B[15000:19800,:]
 from keras.layers import Dense, Dropout, Activation, Flatten, Reshape, LSTM
 from keras.layers import Convolution1D, MaxPooling1D
 from keras.optimizers import SGD as SGD
+from keras.optimizers import Adam
 from keras.utils.np_utils import to_categorical
 arrA = np.asarray(x_train).flatten()
 arrB = np.asarray(y_train).flatten()
-arrA = arrA.reshape(15000, 1, 4000)
-target = y_train.reshape(15000, 1, 6)
+arrA = arrA.reshape(train_len, 1, 4000)
+target = y_train.reshape(train_len, 1, 6)
+xtest = np.asarray(x_test).flatten()
+xtest = xtest.reshape(19800-train_len, 1, 4000)
+ytest =  y_test.reshape(19800-train_len, 1, 6)
 
-#model.add(Reshape(1, 4000, 256), input_shape=4000)
-#model.add(Convolution1D(10, 10))
-
-#model.add(Convolution1D(32, 3, activation='relu'))
-#model.add(MaxPooling1D(pool_size=2))
-#model.add(Dropout(0.25))
-#model.add(Flatten())
-#model.add(Dense(128, activation='relu'))
-#model.add(Dropout(0.5))
-
-#model.add(Dense(units=64, activation='relu', input_dim=4000))
-#Denna funkar inte just nu
-#model.add(LSTM(128, input_shape=4000))
-#model.add(Dense(128, activation='linear'))
-#model.add(Dropout(0.3))
-#model.add(Dense(256))
-#model.add(Dense(32))
-#model.add(Dropout(0.1))
-batch_size = 1
 model = Sequential()
-model.add(LSTM(6, return_sequences=True, input_shape=(1, 4000), activation='sigmoid'))
-model.add(LSTM(6, return_sequences=True, input_shape=(1, 4000), activation='sigmoid'))
-model.add(Dropout(0.2))
-#model.add(Dense(6))
-#model.compile(loss='mean_squared_error', optimizer='adam')
+model.add(LSTM(6, return_sequences=True, input_shape=(1, 4000), activation='softmax'))
+model.add(LSTM(6, return_sequences=True, input_shape=(1, 4000), activation='softmax'))
+model.add(LSTM(6, return_sequences=True, input_shape=(1, 4000), activation='softmax'))
 
 
+model.add(Dense(256))
+model.add(Dropout(0.3))
+model.add(Dense(64))
+model.add(Dropout(0.1))
+model.add(Dense(32))
 model.add(Dense(units=6, activation='softmax'))
-optimizer = SGD(lr = 0.1)
+
+optimizer = Adam(lr=0.00001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
 model.compile(loss='categorical_crossentropy',
               optimizer=optimizer,
-              metrics=['acc'])
-model.fit(arrA, target, nb_epoch=100, batch_size=50, verbose=2)
-#model.compile(loss=keras.losses.categorical_crossentropy,
-#              optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
-              
-#model.fit(x_train, y_train, epochs=12, batch_size=512)
+              metrics=['categorical_accuracy', 'acc'])
+model.fit(arrA, target, nb_epoch=10, batch_size=64, verbose=2)
 
-#loss_and_metrics = model.evaluate(x_test, y_test, batch_size=512)
-#print(loss_and_metrics)
+
+loss_and_metrics = model.evaluate(xtest, ytest, batch_size=64)
+print(loss_and_metrics)

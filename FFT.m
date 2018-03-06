7385ba0570b30256130d35e28a0562a0c3799b7b
Lovisa <liia@exuvo.se>
2018-03-06 19:56:25 +0100

    Update FFT.m

--- FFT.m
+++ FFT.m
@@ -1,7 +1,8 @@
-load rat3_all.mat
-eeg = EEGandEMG;
+clear all, close all;
+r =  matfile('rat3_all.mat', 'Writable', true);
+eeg = r.EEGandEMG;
 [b, a] = butter(7, 0.3, 'high');
 eeg = filter(b, a, eeg);
 eeg = fft(eeg);
 
-EEGandEMG = eeg;
+r.EEGandEMG = eeg;

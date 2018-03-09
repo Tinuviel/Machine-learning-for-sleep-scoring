03e802a78b4ada1a5be2f54ed6cafbcfacabe7f2
Lovisa <liia@exuvo.se>
2018-03-09 20:16:26 +0100

    Add files via upload

---
+++ std1.m
@@ -0,0 +1,19 @@
+r =  matfile('rat3_all.mat', 'Writable', true);
+
+eeg = r.EEGandEMG;
+eeg = eeg - mean(eeg);
+mean(eeg);
+std(eeg);
+eeg2 = eeg./std(eeg);
+std(eeg2);
+r.EEGandEMG = eeg2;
+
+
+%r.EEGandEMG = eeg;
+% matrixA = [0 1 2; 2 7 4; 5 1 7]
+% mean(matrixA)
+% matrixB = matrixA-mean(matrixA)
+% mean(matrixB)
+% std(matrixA)
+% matrixB = matrixA./std(matrixA)
+% std(matrixB)
